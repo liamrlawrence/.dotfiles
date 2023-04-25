e2c6f316ae7584da653f00e6c9a7b4aa8f49c391
Liam Lawrence <liamrlawrence@gmail.com>
2023-04-25 13:08:22 -0400

    Updated after/plugin/harpoon.lua

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -4,7 +4,6 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<leader>A", mark.rm_file)
 
 vim.keymap.set("n", "<C-[>", ui.nav_prev)
 vim.keymap.set("n", "<C-]>", ui.nav_next)
