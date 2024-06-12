7de017242d5485d59799e4f4665412a6fe79ae1a
Liam Lawrence <liamrlawrence@gmail.com>
2024-06-12 14:47:54 -0400

    Moved cloaking autocmd to the plugin's main branch

--- nvim/lua/liamrlawrence/plugins/cloak.lua
+++ nvim/lua/liamrlawrence/plugins/cloak.lua
@@ -16,31 +16,11 @@ return {
                     },
                 },
             },
+            cloak_on_leave = true,          -- ;)
         })
 
-
-        local cloak_group = vim.api.nvim_create_augroup("LL.plugins_cloak-group", { clear = true })
-        local cloak_disabled_buffers = {}
-
-        vim.keymap.set("n", "<leader>es", vim.cmd.CloakPreviewLine)
-        vim.keymap.set("n", "<leader>eS", function()
-            vim.cmd.CloakToggle()
-            -- Make sure that Cloak is re-enabled after leaving the file
-            local bufnr = vim.api.nvim_get_current_buf()
-            if not cloak_disabled_buffers[bufnr] then
-                local autocmd_id = vim.api.nvim_create_autocmd("BufWinLeave", {
-                    desc = "Re-enable Cloak after leaving a file that was disabled",
-                    group = cloak_group,
-                    buffer = bufnr,
-                    callback = function()
-                        vim.cmd.CloakEnable()
-                        vim.api.nvim_del_autocmd(cloak_disabled_buffers[bufnr])
-                        cloak_disabled_buffers[bufnr] = nil
-                    end,
-                })
-                cloak_disabled_buffers[bufnr] = autocmd_id
-            end
-        end, { desc = "Toggle cloaking secrets" })
+        vim.keymap.set("n", "<leader>ec", vim.cmd.CloakPreviewLine, { desc = "Preview cloaked line" })
+        vim.keymap.set("n", "<leader>eC", vim.cmd.CloakToggle,      { desc = "Toggle Cloak" })
     end
 }
 
