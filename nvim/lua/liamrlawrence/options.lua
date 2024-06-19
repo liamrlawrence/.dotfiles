f02692ed28508e141f360d89a5f32d123c22b5c0
Liam Lawrence <liamrlawrence@gmail.com>
2024-06-19 14:11:15 -0400

    Improved Nvim sessions
    - Shadafile is now set to either a global file, or to .git/data.shada
      if `vim .` was run in a directory containing a git project

--- nvim/lua/liamrlawrence/options.lua
+++ nvim/lua/liamrlawrence/options.lua
@@ -17,6 +17,7 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
 
+vim.opt.shadafile = "NONE"
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
--- nvim/lua/liamrlawrence/sessions.lua
+++ nvim/lua/liamrlawrence/sessions.lua
@@ -3,12 +3,17 @@ local session_group = augroup("LL.session-group", { clear = true })
 
 
 vim.api.nvim_create_autocmd("VimEnter", {
-    desc = "Restore vim session",
+    desc = "Initialize vim session",
     group = session_group,
     nested = true,
     callback = function()
+        local session_file = nil
+        local shada_file = vim.fn.stdpath("data") .. "/sessions/global_data.shada"
+
+        -- Load session if 'vim .' was run in a directory containing a git project
         if (vim.fn.isdirectory(".git") == 1) and (vim.fn.argc() == 1 and vim.fn.argv(0) == ".") then
-            local session_file = ".git/session.vim"
+            session_file = ".git/session.vim"
+            shada_file = ".git/data.shada"
 
             if vim.fn.filereadable(session_file) == 1 then
                 vim.cmd("source " .. session_file)
@@ -21,6 +26,12 @@ vim.api.nvim_create_autocmd("VimEnter", {
                     vim.cmd("mksession! " .. session_file)
                 end,
             })
+        end
+
+        -- Load shada
+        vim.opt.shadafile = shada_file
+        if vim.fn.filereadable(shada_file) == 1 then
+            vim.cmd("rshada!")
         end
     end,
 })
