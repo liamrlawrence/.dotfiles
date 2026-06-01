4cb868b7f21599414ad1ccff69410cc3e54c3b02
Liam Lawrence <liamrlawrence@gmail.com>
2026-06-01 10:36:48 -0400

    revert(nvim): reverted highlight-colors plugin to main repository
    - Reverted plugin back to brenoprata10's branch, removed filetype
      options. My fork had fallen too far behind master.

--- nvim/lua/liamrlawrence/plugins/highlight-colors.lua
+++ nvim/lua/liamrlawrence/plugins/highlight-colors.lua
@@ -1,19 +1,9 @@
 return {
-    "liamrlawrence/nvim-highlight-colors",
-    branch = "filetype-options",
+    "brenoprata10/nvim-highlight-colors",
 
     config = function()
         require("nvim-highlight-colors").setup({
             render = "background",
-            filetypes = {
-                "html",
-                "templ",
-                css = {
-                    render = "background",
-                    enable_tailwind = true,
-                },
-                "lua",
-            },
         })
     end
 }
