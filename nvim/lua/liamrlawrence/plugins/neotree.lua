2b3ebcf02891a6b9c14d676611d3269e10b2f8d2
Liam Lawrence <liamrlawrence@gmail.com>
2026-06-01 12:49:10 -0400

    feat(nvim): brought back neo-tree plugin after repossession.nvim update
    - Had to fix repossession.nvim and now neo-tree is working again

--- nvim/lua/liamrlawrence/plugins/neotree.lua
+++ nvim/lua/liamrlawrence/plugins/neotree.lua
@@ -1,15 +1,20 @@
 return {
-    -- "nvim-neo-tree/neo-tree.nvim",
-    -- branch = "v3.x",
-    -- dependencies = {
-    --     "nvim-lua/plenary.nvim",
-    --     "nvim-tree/nvim-web-devicons",
-    --     "MunifTanjim/nui.nvim",
-    -- },
-    --
-    -- config = function ()
-    --     -- vim.keymap.set("n", "<leader>pt", "<cmd>Neotree toggle right<CR>",          { desc = "Neotree toggle right" })
-    --     -- vim.keymap.set("n", "<leader>pT", "<cmd>Neotree float reveal toggle<CR>",   { desc = "Neotree toggle floating" })
-    -- end
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    lazy = false,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+
+    config = function ()
+        require("neo-tree").setup({
+            auto_clean_after_session_restore = true,
+        })
+
+        vim.keymap.set("n", "<leader>pt", "<cmd>Neotree toggle right<CR>",          { desc = "Neotree toggle right" })
+        vim.keymap.set("n", "<leader>pT", "<cmd>Neotree float reveal toggle<CR>",   { desc = "Neotree toggle floating" })
+    end
 }
 
