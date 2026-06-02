267fefe8b3f96de578d8213b17f4a272967c2086
Liam Lawrence <liamrlawrence@gmail.com>
2026-06-01 21:26:11 -0400

    feat(nvim): keybind `<leader>rp` for session manager

--- nvim/lua/liamrlawrence/plugins/repossession.lua
+++ nvim/lua/liamrlawrence/plugins/repossession.lua
@@ -2,7 +2,11 @@ return {
     "liamrlawrence/repossession.nvim",
 
     config = function()
-        require("repossession").setup()
+        require("repossession").setup({
+            tidy_sessions = false,
+        })
+
+        vim.keymap.set("n", "<leader>rp", "<cmd>Repossession<cr>", { desc = "Session manager" })
     end,
 }
 
