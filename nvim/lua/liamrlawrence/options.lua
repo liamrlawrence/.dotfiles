c9a0c5e825e7c23ebe8a16a3e6efe8041f9297d1
Liam Lawrence <liamrlawrence@gmail.com>
2026-06-03 09:55:55 -0400

    refactor(nvim): reorganize options into labeled sections
    - sessionoptions setup so hidden buffers are not saved
    - moved undodir to `vim.fn.stdpath("data")` rather than `$HOME/.vim`
    - increased updatetime from 50ms to 100ms

--- nvim/lua/liamrlawrence/options.lua
+++ nvim/lua/liamrlawrence/options.lua
@@ -1,44 +1,54 @@
 -- The holiest of leader keys
 vim.g.mapleader = " "
 
-vim.opt.statusline = "%<%f %h%m%r%=%-18( 0x%B | %l,%c%V%) %P"
-
+-- UI
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+vim.opt.statusline = "%<%f %h%m%r%=%-18( 0x%B | %l,%c%V%) %P"
 vim.opt.fillchars = { vert = " ", eob = "~" }
-
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80,120"
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
+vim.opt.scrolloff = 8
 
+-- Line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+
+-- Wrapping
 vim.opt.wrap = false
 vim.opt.linebreak = true
 
-vim.opt.shadafile = "NONE"
+-- Search
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Files
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undodir"
 vim.opt.undofile = true
 vim.opt.undolevels = 100000
 
-vim.opt.inccommand = "split"
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
-vim.opt.smartcase = true
-vim.opt.ignorecase = true
+-- Sessions
+vim.opt.sessionoptions = "blank,curdir,folds,help,tabpages,winsize,terminal"
+vim.opt.shadafile = "NONE"
 
-vim.opt.signcolumn = "yes"
-vim.opt.colorcolumn = "80,120"
+-- Performance
+vim.opt.updatetime = 100
 
-vim.opt.scrolloff = 8
-vim.opt.updatetime = 50
-vim.opt.termguicolors = true
-
+-- Clipboard
 vim.g.clipboard = {
     name = "SSH Clipboard",
     copy = {
