bd006e3e8b5077cab345e1e425d1396055b6bfd1
Liam Lawrence <liamrlawrence@gmail.com>
2026-06-08 18:41:28 -0400

    feat(nvim): new keybind `<leader>rl` toggle to last session

--- nvim/lua/liamrlawrence/plugins/repossession.lua
+++ nvim/lua/liamrlawrence/plugins/repossession.lua
@@ -6,7 +6,8 @@ return {
             tidy_sessions = true,
         })
 
-        vim.keymap.set("n", "<leader>rp", "<cmd>Repossession<cr>", { desc = "Session manager" })
+        vim.keymap.set("n", "<leader>rp", "<cmd>Repossession<cr>",      { desc = "Session manager" })
+        vim.keymap.set("n", "<leader>rl", "<cmd>Repossession last<cr>", { desc = "Toggle to last session" })
     end,
 }
 
