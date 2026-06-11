cc2830ec14bd8540627ecbce8cae71b55873dab2
Liam Lawrence <liamrlawrence@gmail.com>
2026-06-11 01:05:13 -0400

    refactor(tmux): move tpm and resurrect dirs to XDG path

--- tmux/scripts/install_tmux.sh
+++ tmux/scripts/install_tmux.sh
@@ -5,6 +5,6 @@
 sudo apt-get update && sudo apt-get install -y tmux
 
 # Plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 tmux source-file ~/.config/tmux/tmux.conf
 
