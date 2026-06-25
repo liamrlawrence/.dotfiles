05a42ead8f459ead120eb0fa5ffb269e73fbed86
Liam Lawrence <liamrlawrence@gmail.com>
2026-06-25 13:07:53 -0400

    feat(nvim): lazy key load undotree, add cleanup on session switch

--- nvim/lua/liamrlawrence/plugins/undotree.lua
+++ nvim/lua/liamrlawrence/plugins/undotree.lua
@@ -1,9 +1,18 @@
 return {
     "mbbill/undotree",
-    event = "VeryLazy",
 
     config = function()
+        local undotree_group = vim.api.nvim_create_augroup("LL.plugins_undotree-group", { clear = true })
+        vim.api.nvim_create_autocmd("User", {
+            desc    = "Clean up undotree UI when leaving a session",
+            group   = undotree_group,
+            pattern = "RepossessionSwitchPre",
+            command = "UndotreeHide",
+        })
+
         vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undotree" })
     end,
+
+    keys = { "<leader>u" },
 }
 
