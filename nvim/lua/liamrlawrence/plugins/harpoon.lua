45679562447134d50dea4b0d83c9e63f1157aa23
Liam Lawrence <liamrlawrence@gmail.com>
2026-06-25 13:07:29 -0400

    feat(nvim): lazy key load harpoon

--- nvim/lua/liamrlawrence/plugins/harpoon.lua
+++ nvim/lua/liamrlawrence/plugins/harpoon.lua
@@ -2,7 +2,6 @@ return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
-    event = "VeryLazy",
 
     config = function()
         local harpoon = require("harpoon")
@@ -12,6 +11,8 @@ return {
         vim.keymap.set("n", "<M-]>",     function() harpoon:list():next()                        end, { desc = "Harpoon next file" })
         vim.keymap.set("n", "<M-[>",     function() harpoon:list():prev()                        end, { desc = "Harpoon prev file" })
         vim.keymap.set("n", "<C-e>",     function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon toggle menu" })
-    end
+    end,
+
+    keys = { "<leader>a", "<M-]>", "<M-[>", "<C-e>" },
 }
 
