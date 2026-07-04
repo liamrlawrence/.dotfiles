bc1ba5d8cc06cad7587c7479a24a2a4854f1efa5
Liam Lawrence <liamrlawrence@gmail.com>
2026-07-04 18:13:38 -0400

    feat(nvim): prevent gitsigns from attaching to org buffers

--- nvim/lua/liamrlawrence/plugins/gitsigns.lua
+++ nvim/lua/liamrlawrence/plugins/gitsigns.lua
@@ -3,7 +3,14 @@ return {
     event = "VeryLazy",
 
     config = function()
-        require("gitsigns").setup()
+        require("gitsigns").setup({
+            on_attach = function(bufnr)
+                local ft = vim.bo[bufnr].filetype
+                if ft == "org" or ft == "orgagenda" then
+                    return false
+                end
+            end,
+        })
 
         vim.keymap.set("n", "<leader>gg", "<cmd>Gitsigns toggle_signs<cr>", { desc = "Toggle Git signs" })
     end,
