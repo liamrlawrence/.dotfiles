8aacb768530c88b0dd9fb79c478005f3cf278c27
Liam Lawrence <liamrlawrence@gmail.com>
2026-08-03 03:43:13 -0400

    feat(nvim): lengthen the smear cursor trail

--- nvim/lua/liamrlawrence/plugins/smear.lua
+++ nvim/lua/liamrlawrence/plugins/smear.lua
@@ -6,8 +6,9 @@ return {
         require("smear_cursor").setup({
             time_interval = 4,
             stiffness = 0.8,
-            trailing_stiffness = 0.5,
-            distance_stop_animation = 0.5,
+            trailing_stiffness = 0.45,
+            damping = 0.85,
+            anticipation = 0.1,
         })
 
         vim.keymap.set("n", "<Leader>es", "<Cmd>SmearCursorToggle<CR>", { desc = "Toggle Smear" })
